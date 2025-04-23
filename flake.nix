bc9ecd59bb6c28d183b4a6d2c27840e0e0fc2ec4
Alex Sampley <alex.sampley@gmail.com>
2025-04-23 15:11:04 -0600

    Support multiple systems

--- flake.nix
+++ flake.nix
@@ -1,68 +1,62 @@
 {
-	description = "Home Manager configuration of asampley";
+  description = "Home Manager configuration of asampley";
 
-	inputs = {
-		# Specify the source of Home Manager and Nixpkgs.
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		nvim-config = {
-			type = "git";
-			# TODO: This will simplify in 2.27
-			url = "file:./files/.config/nvim";
-			flake = false;
-		};
-		awesome-config = {
-			type = "git";
-			# TODO: This will simplify in 2.27
-			url = "file:./files/.config/awesome";
-			submodules = true;
-			flake = false;
-		};
-	};
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-	outputs = { nixpkgs, home-manager, nvim-config, awesome-config, ... }:
-		let
-			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
-		in rec {
-			# Fallback system that assumes no features available
-			homeConfigurations."asampley" = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
+    systems = {
+      url = ./systems.nix;
+      flake = false;
+    };
 
-				# Specify your home configuration modules here, for example,
-				# the path to your home.nix.
-				modules = [ ./home.nix ];
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-				# Optionally use extraSpecialArgs
-				# to pass through arguments to home.nix
-				extraSpecialArgs = {
-					inherit nvim-config;
-					inherit awesome-config;
-					gui = false;
-					x = false;
-					wine = false;
-				};
-			};
+    nvim-config = {
+      type = "git";
+      # TODO: This will simplify in 2.27
+      url = "file:./files/.config/nvim";
+      flake = false;
+    };
 
-			homeConfigurations."asampley@amanda" = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
+    awesome-config = {
+      type = "git";
+      # TODO: This will simplify in 2.27
+      url = "file:./files/.config/awesome";
+      submodules = true;
+      flake = false;
+    };
+  };
 
-				# Specify your home configuration modules here, for example,
-				# the path to your home.nix.
-				modules = [ ./home.nix ];
+  outputs = { nixpkgs, systems, home-manager, nvim-config, awesome-config, ... }: {
+    # Using packages.${system}.homeConfigurations allows us to build for many targets
+    packages = nixpkgs.lib.genAttrs (import systems) (system: rec {
+      # Fallback system that assumes no features available
+      homeConfigurations."asampley" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
 
-				# Optionally use extraSpecialArgs
-				# to pass through arguments to home.nix
-				extraSpecialArgs = {
-					inherit nvim-config;
-					inherit awesome-config;
-					gui = true;
-					x = true;
-					wine = true;
-				};
-			};
-		};
+        # Specify your home configuration modules here, for example, the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs to pass through arguments to home.nix
+        extraSpecialArgs = {
+          inherit nvim-config;
+          inherit awesome-config;
+          gui = false;
+          x = false;
+          wine = false;
+        };
+      };
+
+      # Home computer with additional features
+      homeConfigurations."asampley@amanda" = homeConfigurations."asampley" // {
+        extraSpecialArgs.gui = true;
+        extraSpecialArgs.x = true;
+        extraSpecialArgs.wine = true;
+      };
+    });
+  };
 }
